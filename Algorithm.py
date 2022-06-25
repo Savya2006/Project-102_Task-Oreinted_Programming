d42adb3c32ba2ece4a033f9f9f095d70da00b517
Savya2006 <87526775+Savya2006@users.noreply.github.com>
2022-06-25 20:22:32 +0530

    Add files via upload

---
+++ Algorithm.py
@@ -0,0 +1,22 @@
+print("Top 5 tips on how to study smarter, not longer~!3")
+tips = input("Enter The Any Number Tip You Want:- ")
+if(tips == '1'):
+    print("1. Pick a place and time")
+    print("Everyone has their own idea about the best place and time to study. Whether it's your bedroom at night or the library after school, find a study space and a regular study time that works for you and stick with it.")
+
+elif(tips == '2'):
+    print("2. Use pictures")
+    print("Pay attention to diagrams and graphs in your class materials, says Nebel. “Those pictures can really boost your memory of this material. And if there aren’t pictures, creating them can be really, really useful.”")
+
+elif(tips == '3'):
+    print("3. Practice, practice, practice!")
+    print("Musicians practice their instruments. Athletes practice sports skills. The same should go for learning.")
+elif(tips == '4'):
+    print("4. Find examples")
+    print("Abstract concepts can be hard to understand. It tends to be far easier to form a mental image if you have a concrete example of something")
+elif(tips == '5'):
+    print("5. Be kind to yourself")
+    print(" Try to stick to a regular routine. And get enough sleep — not just the night before the test but for weeks or months on end. “Those things are really, really important for learning”.Exercise helps as well.")
+
+if(tips >= '6'):
+    print("Sorry! There are only 5 tips.")
